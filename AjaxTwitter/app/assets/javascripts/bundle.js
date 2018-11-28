c64e60a0d5680045bd3dbe96958f1fa59e53a95c
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-28 14:32:58 -0800

    Add event listener

--- AjaxTwitter/app/assets/javascripts/bundle.js
+++ AjaxTwitter/app/assets/javascripts/bundle.js
@@ -99,6 +99,7 @@ class FollowToggle {
     this.followState = $(el).data('initial-follow-state');
     this.$el = $(el);
     this.render();
+    this.handleClick();
   }
 
   render() {
@@ -107,6 +108,38 @@ class FollowToggle {
     } else {
       this.$el.html("Unfollow!");
     }
+  }
+
+  handleClick(){
+
+    debugger;
+
+    this.$el.on("click", e => {
+      debugger;
+      let method;
+      if(this.followState === "unfollowed") {
+        method = "POST";
+      } else {
+        method = "DELETE";
+      }
+      e.preventDefault();
+      return $.ajax({
+        method: method,
+        url: `/users/${this.userId}/follow`,
+        dataType: 'JSON'
+      }).then(() => {
+        if(this.followState === "unfollowed") {
+          this.followState = "followed";
+        } else {
+          this.followState = "unfollowed";
+        }
+        this.render();
+      });
+    });
+    // debugger
+
+
+    // this.render();
   }
 }
 
--- AjaxTwitter/frontend/follow_toggle.js
+++ AjaxTwitter/frontend/follow_toggle.js
@@ -4,6 +4,7 @@ class FollowToggle {
     this.followState = $(el).data('initial-follow-state');
     this.$el = $(el);
     this.render();
+    this.handleClick();
   }
 
   render() {
@@ -12,6 +13,38 @@ class FollowToggle {
     } else {
       this.$el.html("Unfollow!");
     }
+  }
+
+  handleClick(){
+
+    debugger;
+
+    this.$el.on("click", e => {
+      debugger;
+      let method;
+      if(this.followState === "unfollowed") {
+        method = "POST";
+      } else {
+        method = "DELETE";
+      }
+      e.preventDefault();
+      return $.ajax({
+        method: method,
+        url: `/users/${this.userId}/follow`,
+        dataType: 'JSON'
+      }).then(() => {
+        if(this.followState === "unfollowed") {
+          this.followState = "followed";
+        } else {
+          this.followState = "unfollowed";
+        }
+        this.render();
+      });
+    });
+    // debugger
+
+
+    // this.render();
   }
 }
 
