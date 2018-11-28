bed0cb7107bad11dab0f817193f8cb0ce983524d
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-28 14:00:55 -0800

    Add text to button

--- AjaxTwitter/app/assets/javascripts/bundle.js
+++ AjaxTwitter/app/assets/javascripts/bundle.js
@@ -98,8 +98,16 @@ class FollowToggle {
     this.userId = $(el).data('user_id');
     this.followState = $(el).data('initial-follow-state');
     this.$el = $(el);
+    this.render();
   }
 
+  render() {
+    if(this.followState === "unfollowed") {
+      this.$el.html("Follow!");
+    } else {
+      this.$el.html("Unfollow!");
+    }
+  }
 }
 
 module.exports = FollowToggle;
