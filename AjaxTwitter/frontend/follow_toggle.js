8c089a3014ae86316ad012287f8dd534134610fa
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-28 12:20:00 -0800

    Create FollowToggle class

--- AjaxTwitter/frontend/follow_toggle.js
+++ AjaxTwitter/frontend/follow_toggle.js
@@ -1,8 +1,10 @@
 class FollowToggle {
   constructor (el) {
-    this.userId = el.user_id;
-    this.followState = el.initial-follow-state;
+    this.userId = $(el).data('user_id');
+    this.followState = $(el).data('initial-follow-state');
     this.$el = $(el);
   }
 
 }
+
+module.exports = FollowToggle;
